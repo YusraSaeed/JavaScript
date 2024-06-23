c67a822e7ae205fdd68a5355438343b117fd2c7c
Yusra Saeed <146421260+YusraSaeed@users.noreply.github.com>
2024-06-23 14:35:09 +0000

    01_variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,16 @@ const accountId = 144553
 let accountEmail = "yusrasaeed333@gmail.com"
 var accountPassword = "12345"
 accountCity = "Nowshera"
+let accountState
 
-accountId = 2
+// accountId = 2  not allowed
+
+accountEmail = "ys@gmail.com"
+accountPassword = "12338757"
+accountCity = "Peshawar"
+
 console.log(accountId);
+
+/* Never use var only use let because var doesn't have any scope and also never declare a variable
+without mentioning its time */
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
