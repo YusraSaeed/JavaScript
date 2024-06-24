5b3db336b47a361827effd1467eca9bb0fa58c78
Yusra Saeed <146421260+YusraSaeed@users.noreply.github.com>
2024-06-24 08:13:41 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat as JS code as newer version
+
+// alert (3  + 3) //this syntax of alert is not usable in node we can use it on browser console and in html
+
+let name = "Yusra"
+let age = 21
+let isLoggedIn = true
+
+// Primitive Datatypes
+// number => 2 power 53 is range
+// bignit ususally not used
+// string can be used in both single and double quotes
+// boolean => true/false
+// null => standalone value means empty
+// undefined => value isn't been assigned yet
+// symbol => uniqueness
+
+
+// objects 
+
+console.log(typeof "yusra");
+console.log(typeof (age)); //or console.log(typeof age)
+console.log(typeof null); // object
+ console.log(typeof undefined); //undefined
